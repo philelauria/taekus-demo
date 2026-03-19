d7d43647b8f729745024cb8e53857faf9365d4ed
Phil Elauria <eudaemon@philMacBookProPersonal.local>
2026-03-19 14:23:30 -0400

    feat: add TanStack Query provider infrastructure
    - Install @tanstack/react-query
    - Create QueryClient with default stale time and retry config
    - Wire QueryClientProvider into App.tsx alongside existing RTK Query
    - No query hooks migrated yet; both caching systems coexist

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import '../../global.css'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { StoreProvider as MobXStoreProvider } from '~/mobxStores/StoreProvider'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '~/api/queryClient'
 import { ThemeProvider } from './providers/ThemeProvider'
 import { Provider as ReduxStoreProvider } from 'react-redux'
 import { store } from '~/store/store'
@@ -11,11 +13,13 @@ const App = () => {
     return (
         <ReduxStoreProvider store={store}>
             <MobXStoreProvider>
-                <SafeAreaProvider>
-                    <ThemeProvider>
-                        <RootNavigator />
-                    </ThemeProvider>
-                </SafeAreaProvider>
+                <QueryClientProvider client={queryClient}>
+                    <SafeAreaProvider>
+                        <ThemeProvider>
+                            <RootNavigator />
+                        </ThemeProvider>
+                    </SafeAreaProvider>
+                </QueryClientProvider>
             </MobXStoreProvider>
         </ReduxStoreProvider>
     )
